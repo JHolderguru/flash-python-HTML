67c02a23496d53114a8a748765d53eaa72f5b62e
JHolderguru <64795186+JHolderguru@users.noreply.github.com>
2020-08-27 13:45:46 +0100

    pages yp and running

---
+++ flask101.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+posts = [
+{
+	'author' :'Jonathan Holder',
+	'title':'Day in the life...',
+	'content':'Day in the life of a DevOps Engineer',
+	'date_posted':'August 2020'
+
+},
+
+]
+
+
+
+
+
+@app.route("/")
+@app.route("/index")
+
+def home():
+    return render_template('index.html',posts=posts)
+
+
+@app.route("/About")
+def about():
+    return  render_template('about.html', title='About')
+
+if __name__=='__main__':
+	app.run(debug=True)
+
+
+
+
+
+
+
+
